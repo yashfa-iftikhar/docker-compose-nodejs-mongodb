bb82687dbc611a446313759d59d504484b766bb5
yashfa-iftikhar <iftikharyashfa888@gmail.com>
2025-05-12 21:38:40 +0500

    Update index.js

--- bezkoder-app/app/models/index.js
+++ bezkoder-app/app/models/index.js
@@ -1,11 +1,23 @@
-const dbConfig = require("../config/db.config.js");
-
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+require("dotenv").config();
+
+const {
+  MONGODB_USER,
+  MONGODB_PASSWORD,
+  MONGODB_DATABASE,
+  MONGODB_LOCAL_PORT,
+  DB_HOST,
+} = process.env;
+
+const host = DB_HOST || "localhost";
+
+const url = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${host}:${MONGODB_LOCAL_PORT}/${MONGODB_DATABASE}?authSource=admin`;
 
 const db = {};
 db.mongoose = mongoose;
-db.url = dbConfig.url;
+db.url = url;
+
 db.tutorials = require("./tutorial.model.js")(mongoose);
 
 module.exports = db;
+
